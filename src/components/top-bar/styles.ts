5a95545b52a8b8ee501593492c0df3357796dc4c
Eugene Molchanov <super.molchanov@list.ru>
2023-03-15 22:35:03 +0600

    Add responsive top bar - Refactoring top bar component

--- src/components/top-bar/styles.ts
+++ src/components/top-bar/styles.ts
@@ -2,18 +2,22 @@ import { Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { tokens } from "../../theme";
 
-
 export const useStyles = makeStyles((theme: Theme) => {
-  const colors = tokens(theme.palette.mode);
-  return {
+	const colors = tokens(theme.palette.mode);
+	return {
 		root: {
-			display: "flex",
-			justifyContent: "space-between",
-			alignItems: "center",
-			padding: "32px 24px",
-			backgroundColor: colors.primary.DEFAULT,
-			maxHeight: "95px",
+			position: "static",
+			background: `${colors.primary.DEFAULT} !important`,
 			borderBottom: `1px solid ${colors.borderColor}`,
+			boxShadow: 'none !important'
+		},
+		toolbar: {
+			justifyContent: "space-between",
+			padding: '25px 45px'
+		},
+		menuIcon: {
+			marginRight: '10px',
+			cursor: 'pointer'
 		},
 		iconBlock: {
 			paddingRight: "37px",
@@ -25,6 +29,7 @@ export const useStyles = makeStyles((theme: Theme) => {
 		},
 		searchBlock: {
 			display: "flex",
+			maxHeight: '45px',
 			borderRadius: "8px",
 			marginLeft: "28px",
 			backgroundColor: `${colors.primary[600]}`,
