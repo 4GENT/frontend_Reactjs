9a025e862b1e0e08ee971c1cd720c121c04f5c29
Eugene Molchanov <super.molchanov@list.ru>
2023-03-19 05:50:57 +0600

    Create area chart component - Create graph component - Add option chart - Add chart data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,54 +1,62 @@
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import React, { FC, useCallback, useEffect, useMemo, useRef } from "react";
 import { useAppDispatch, useAppSelector } from "../../utils/hook";
 import { getFavoriteAssets } from "../../store/thunks/assests";
 import { Box, Grid } from "@mui/material";
 import { useStyles } from "./styles";
+import AreaChart from "../../components/charts/area-chart";
 
-const Home = () => {
-  const favoriteAssets: any[] = useAppSelector(state => state.assets.favoriteAssets)
-  const dispatch = useAppDispatch()
-  const fetchDataRef = useRef(false)
+const Home: FC = (): JSX.Element => {
+  const favoriteAssets: any[] = useAppSelector(
+    (state) => state.assets.favoriteAssets,
+  );
+  const dispatch = useAppDispatch();
+  const fetchDataRef = useRef(false);
   const classes = useStyles();
 
-  const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], []);
-  const filteredArray = favoriteAssets.filter((value, index, self) => index === self.findIndex((t) => t.name === value.name))
-
+  const favoriteAssetName = useMemo(() => ["bitcoin", "ethereum"], []);
+  const filteredArray = favoriteAssets.filter(
+    (value, index, self) =>
+      index === self.findIndex((t) => t.name === value.name),
+  );
   const fetchData = useCallback(
-		(data: string[]) => {
-			data.forEach((element: string) => {
-				dispatch(getFavoriteAssets(element));
-			});
-		}, [dispatch]);
+    (data: string[]) => {
+      data.forEach((element: string) => {
+        dispatch(getFavoriteAssets(element));
+      });
+    },
+    [dispatch],
+  );
 
   useEffect(() => {
-		if (fetchDataRef.current) return
-		fetchDataRef.current = true
-		fetchData(favoriteAssetName)
-	}, [favoriteAssetName, fetchData])
+    if (fetchDataRef.current) return;
+    fetchDataRef.current = true;
+    fetchData(favoriteAssetName);
+  }, [favoriteAssetName, fetchData]);
 
   const renderFavoriteBlock = filteredArray.map((element: any) => {
-      const currentPrice = element.data.prices[0];
-      const currentCap = element.data.market_caps[0];
-      return (
+    console.log('Element', element)
+    const currentPrice = element.data.prices[0];
+    const currentCap = element.data.market_caps[0];
+    return (
+      <Grid item xs={12} sm={6} lg={6} key={element.name}>
+        <Grid container className={classes.topCardItem}>
           <Grid item xs={12} sm={6} lg={6}>
-              <Grid container className={classes.topCardItem}>
-                  <Grid item xs={12} sm={6} lg={6}>
-                      <h3 className={classes.assetName}>{element.name}</h3>
-                      <div className={classes.itemDetails}>
-                          <h3 className={classes.cardPrice}>
-                              ${currentPrice[1].toFixed(2)}
-                          </h3>
-                          <p className={classes.cardCapitalize}>
-                              ${currentCap[1].toFixed(0)}
-                          </p>
-                      </div>
-                  </Grid>
-                  <Grid item xs={12} sm={6} lg={6}>
-                      <h5>Chart</h5>
-                  </Grid>
-              </Grid>
+            <h3 className={classes.assetName}>{element.name}</h3>
+            <div className={classes.itemDetails}>
+              <h3 className={classes.cardPrice}>
+                ${currentPrice[1].toFixed(2)}
+              </h3>
+              <p className={classes.cardCapitalize}>
+                ${currentCap[1].toFixed(0)}
+              </p>
+            </div>
           </Grid>
-      );
+          <Grid item xs={12} sm={6} lg={6}>
+            <AreaChart data={element.data.prices} />
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   });
 
   return (
@@ -57,7 +65,7 @@ const Home = () => {
         {renderFavoriteBlock}
       </Grid>
     </Box>
-  )
-}
+  );
+};
 
 export default Home;
